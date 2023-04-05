739b7d5bc3b8034ec9375887d5453226ada6b4a4
stuart howarth <stuarthowarth88@gmail.com>
2023-04-05 20:35:17 +0100

    model details .py layout updated to import load time

--- airline_sentiment/streamlit/pages/Model details.py
+++ airline_sentiment/streamlit/pages/Model details.py
@@ -6,6 +6,15 @@ from airline_sentiment.ml_logic.data import clean
 from tensorflow.keras import models
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+# load model
+model = models.load_model('models/models.h5')
+
+# load tokenizer
+with open('tokenizer/tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+airport_code_list = list(pd.read_csv('data/raw_data/airline_codes.csv')['ICAO'].dropna())
+
 st.set_page_config(page_title="Model Demo",)
 
 st.markdown("# Model Demo")
@@ -19,16 +28,7 @@ if st.button('click me'):
 
     print('button clicked!')
 
-    airport_code_list = list(pd.read_csv('data/raw_data/airline_codes.csv')['ICAO'].dropna())
-
     X_pred = clean(title, airport_code_list)
-
-    # load model
-    model = models.load_model('models/models.h5')
-
-    # load tokenizer
-    with open('tokenizer/tokenizer.pickle', 'rb') as handle:
-        tokenizer = pickle.load(handle)
 
     X_test_token = tokenizer.texts_to_sequences([X_pred])
 
