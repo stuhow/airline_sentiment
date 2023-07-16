1b648e1f217f39fb182d6d01697e6c910533d5d9
stuart howarth <stuarthowarth88@gmail.com>
2023-07-16 16:37:33 +0100

    Model.py page of my streamlit app will now make predictions, could still do with some UI changes

---
+++ airline_sentiment/streamlit/pages/Model.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import requests
+
+st.write("## Test out the models predictions")
+st.write("### Enter a tweet below and click on make prediction!")
+
+with st.form(key='params_for_api'):
+    example_text = st.text_input("Enter some text 👇", "the worst flight i've ever taken")
+    submit_button = st.form_submit_button('Make prediction')
+
+if submit_button:
+    params = dict(text=example_text)
+    api_url = 'https://sentimentapi-ptw3tzx4aq-ew.a.run.app/predict'
+    response = requests.get(api_url, params=params)
+    prediction = response.json()
+    pred = prediction['prediction']
+    st.header(f'This is a {pred}')
